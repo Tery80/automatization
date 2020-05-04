0dfe0a51aebe6c6f118e3bb6c10f48d6f5d207c8
Jekaterina <35897771+Tery80@users.noreply.github.com>
2020-05-04 14:38:35 +0300

    04.05

--- src/test/java/stepDefs/WeatherStepDefs.java
+++ src/test/java/stepDefs/WeatherStepDefs.java
@@ -1,15 +1,22 @@
 package stepDefs;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import model.Response;
+import model.Weather;
+import org.junit.jupiter.api.Assertions;
 import requesters.WeatherRequester;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class WeatherStepDefs {
     private String cityName;
     private String country;
-
+    private Response response;
     @Given("show test name (.*)")
     public void show_test_name(String annotation){
         System.out.println(annotation);
@@ -26,20 +33,33 @@ public class WeatherStepDefs {
     }
 
     @When ("we are requesting weather data")
-     public void request_weather(){
+    public void request_weather() throws JsonProcessingException {
         WeatherRequester requester = new WeatherRequester();
-        Response response = requester.requestWeather(cityName, country);
-        System.out.println(response.getCoord().getLat());
-        //System.out.println(requester.requestWeather(cityName, country));
-            }
+        response = requester.requestWeather(cityName, country);
+
+    }
 
     @Then("lon is {float}")
     public void check_lon(float lon){
-
+        assertEquals(lon,response.getCoord().getLon(), "wrong lon");
     }
 
     @Then("lat is {float}")
     public void check_lat(float lat){
-
+        assertEquals(lat, response.getCoord().getLat(), "wrong lat");
     }
+
+    @Then("weather is:")
+    public void check_weather(Map<String, String> params) {
+        Weather weather= response.getWeathers().get(0);
+        assertEquals(Long.parseLong(params.get("id")),weather.getId(), "wrong weather id");
+        assertEquals(params.get("main"), weather.getMain(),"incorrect weather main");
+        assertEquals(params.get("description"),weather.getDescription(),"incorrect weather description");
+        assertEquals(params.get("icon"), weather.getIcon(), "incorrect weather item");
+    }
+     @Then("base is: {string}")
+    public void check_base(String base){
+        assertEquals(base, response.getBase(), "wrong base");
+     }
+
 }
